35c186c768226ac9e5020c910c843744c27b6db9
HuamanAngel <oxipusioxx12@gmail.com>
2022-03-13 12:12:09 -0500

    Agregado estructura

---
+++ course/models.py
@@ -0,0 +1,54 @@
+from django.db import models
+from django.urls import reverse
+from django.contrib.auth.models import User
+import uuid
+
+
+from ckeditor.fields import RichTextField
+
+def user_directory_path(instance, filename):
+    # THis file will be uploaded to MEDIA_ROOT /the user_(id)/the file
+    return 'user_{0}/{1}'.format(instance.user.id, filename)
+
+
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    
+class Course(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    picture = models.ImageField(upload_to=user_directory_path)
+    title = models.CharField(max_length=200)
+    description = models.CharField(max_length=300)
+    DAY_CHOICES = [
+        ('LU', 'Lunes'),
+        ('MA', 'Martes'),
+        ('MI', 'Miércoles'),
+        ('JU', 'Jueves'),
+        ('VI', 'Viernes'),
+        ('SA', 'Sábado'),
+        ('DO', 'Domingo')
+    ]
+    day = models.CharField(
+        max_length=2,
+        choices=DAY_CHOICES,
+        default='LU'
+    )
+    time_start = models.TimeField()
+    time_end = models.TimeField()
+    # category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    syllabus = RichTextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='course_owner')
+    enrolled = models.ManyToManyField(User)
+    # modules = models.ManyToManyField(Module)
+    questions = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.title    
---
+++ student_portal/view.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from django.http import HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+
+def saludo(request):
+    return HttpResponse("Hola alumnos esta es nuestra primera pagina con Django")
+
+
+def despedida(request):
+    
+    # return render(request,'student_portal')
+    # dateNow = datetime.now()
+    # documento1= """
+    #     <h1> %s </h1>
+    # """ % dateNow
+    
+    # # doc_externo = open("/home/jose/Documentos/hola.txt", "r")
+    return render(request,'courses/allCourses.html',{})
+
