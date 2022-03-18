bbc882240ad8c3bdf13e2373a655c62e29520df5
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2022-03-18 13:55:16 -0500

    correccion de ubicacion de archivo login.html

--- authapp/urls.py
+++ authapp/urls.py
@@ -6,6 +6,6 @@ from django.contrib.auth.views import (LoginView, LogoutView)
 
 urlpatterns = [
 	# path('login/', loginView, name="login"),
-	path('login/', LoginView.as_view(template_name='auth/login.html'), name='login'),
-	path('logout/', LogoutView.as_view(template_name='auth/logged_out.html') , name="logout"),
+	path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+	path('logout/', LogoutView.as_view(template_name='registration/logged_out.html') , name="logout"),
 ]
