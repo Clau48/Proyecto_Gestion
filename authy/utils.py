4cfc01f712ab6fc0ea5146080769c96443285695
LuiggiPasacheL <luiggi.pasache@unmsm.edu.pe>
2022-03-26 16:24:00 -0500

    Agregando plantilla html para correo de autenticacion

--- authy/utils.py
+++ authy/utils.py
@@ -4,6 +4,7 @@ from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes
 from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import send_mail
+from django.utils.html import strip_tags
 from dotenv import load_dotenv
 import os
 
@@ -11,7 +12,7 @@ def send_email_confirmation(request, user):
 	if user.email is not None:
 		current_site = get_current_site(request)
 		subject = 'Activa tu cuenta en Fisiversity'
-		message = render_to_string('registration/email_confirmation.html',
+		html_message = render_to_string('registration/email_confirmation.html',
 			{
 				"domain": current_site.domain,
 				"user": user,
@@ -20,7 +21,8 @@ def send_email_confirmation(request, user):
 			},
 		)
 		to_email = user.email
-		return send_mail(subject, message, os.getenv('EMAIL_HOST_USER') , [to_email])
+		plain_message = strip_tags(html_message)
+		return send_mail(subject, plain_message, os.getenv('EMAIL_HOST_USER') , [to_email], html_message=html_message)
 	else:
 		print('Error: Usuario sin email, no se puede enviar')
 		return 0
