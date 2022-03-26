e2b3f1517c9ccbc6ba34dbd504d738cfb4ea8521
HuamanAngel <oxipusioxx12@gmail.com>
2022-03-26 16:12:55 -0500

    Corregido url de archivos e imagenes, interfaces

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -128,9 +128,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'templates/static')
 ]
-
+# BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
 
 
 #Login
--- student_portal/urls.py
+++ student_portal/urls.py
@@ -19,6 +19,9 @@ from django.urls import path, include
 from student_portal.view import *;
 from authy.views import *
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('', showCourse, name='index'),
 
@@ -30,3 +33,5 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
