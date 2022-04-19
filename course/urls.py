53d132ae710f0e16758b8c90f1cc25f46ad4a696
Alexis Arias <alexis.arias555@gmail.com>
2022-04-19 17:19:41 -0500

    agregar edicion de posts

--- course/urls.py
+++ course/urls.py
@@ -5,17 +5,28 @@ from course.views import  *
 urlpatterns = [
 	#Course - Classroom Views
 	path('', showCourse, name='show_courses'),
+	
+	#Course routes
 	path('newcourse/', new_course, name='newcourse'),
 	path('mycourses/', show_mycourses, name='mycourses'),
 	path('<course_id>/', show_course_description, name='show_description'),
 	path('<course_id>/posts/notas', show_calification, name='show_calification'),
+
+	#Post routes
 	path('<course_id>/posts', show_posts, name='show_posts'),
 	path('<course_id>/posts/newpost', NewPost, name='new-post'),
+	path('<course_id>/posts/<post_id>/editpost', edit_post, name='edit-post'),
+
+	#Homework routes
 	path('<course_id>/<post_id>/entrega', send_homework, name='send_homework'), 
- 	path('<course_id>/<post_id>/entrega/process', send_homework_post, name='send_homework_post'), 
+ 	path('<course_id>/<post_id>/entrega/process', send_homework_post, name='send_homework_post'),
+
+	#Auth routes
 	path("<course_id>/users/", usersInCourse, name="users_in_course"),
 	path("<course_id>/send_invitation", sendInscriptionLink, name="send_inscription"),
 	path("inscription/<codeInvitation>/", inscriptionLink, name="url_inscription"),
+
+	#Assignment routes
 	path('<course_id>/posts/newassignment', new_assignment, name='new-assignment'),
 	path('<course_id>/posts/<assignment_id>/editassignment', edit_assignment, name='edit-assignment'),
 ]
